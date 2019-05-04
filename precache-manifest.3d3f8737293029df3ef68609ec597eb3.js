61e2cddfc11dbf202cb3c8a7978f3ec1842cb982
Bayu Lalu <bayulalu3@gmail.com>
2019-05-04 12:29:44 +0700

    pase 1

---
+++ precache-manifest.3d3f8737293029df3ef68609ec597eb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9dd947fe0462f91c5b68",
+    "url": "/static/js/main.f21f36a2.chunk.js"
+  },
+  {
+    "revision": "149bab0b4344b6447474",
+    "url": "/static/js/2.4ce2bffc.chunk.js"
+  },
+  {
+    "revision": "9dd947fe0462f91c5b68",
+    "url": "/static/css/main.1e781b5a.chunk.css"
+  },
+  {
+    "revision": "149bab0b4344b6447474",
+    "url": "/static/css/2.6602667a.chunk.css"
+  },
+  {
+    "revision": "f9ab453916c923944c3617c1a88b11a2",
+    "url": "/index.html"
+  }
+];
